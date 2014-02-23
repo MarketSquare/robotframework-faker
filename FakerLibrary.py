aac7422f97b39f3464398ddde606bf93be8ce92c
Guy Kisel <gkisel@truecar.com>
2014-02-22 18:54:52 -0800

    FakerLibrary

---
+++ FakerLibrary.py
@@ -0,0 +1,19 @@
+import faker
+
+"""
+
+This is a very thin wrapper for faker. You can access all of faker's usual methods
+via FakerLibrary calls in Robot Framework.
+
+"""
+
+class FakerLibrary(object):
+    def __init__(self, **kwargs):
+        # create our faker
+        self._fake = faker.Factory.create(**kwargs)
+
+        # set all of the faker's public methods to be our methods
+        for method_name, method in self._fake.__dict__.items():
+            if not method_name[0] == '_':
+                setattr(self, method_name, method)
+

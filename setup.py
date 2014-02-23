2150252c173b287cfbefa6c01095689a16ad953f
Guy Kisel <gkisel@truecar.com>
2014-02-22 19:56:29 -0800

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-#!/usr/bin/env python
 
 from distutils.core import setup
 
 setup(
-    name='robotframework-ride',
-    packages=['robotframework-ride'],  # this must be the same as the name above
+    name='robotframework-faker',
+    packages=['robotframework-faker'],  # this must be the same as the name above
     version='0.1',
     description='Robot Framework wrapper for faker, a fake test data generator',
     author='Guy Kisel',
     author_email='guy.kisel@gmail.com',
     url='https://github.com/guykisel/robotframework-faker',
-    keywords=['testing', 'faker'],  # arbitrary keywords
+    keywords='robotframework testing test automation faker',  # arbitrary keywords
     classifiers=[],
     install_requires=['fake-factory', 'robotframework'],
+    long_description=open('README.md').read(),
+    license=open('LICENSE').read(),
 )

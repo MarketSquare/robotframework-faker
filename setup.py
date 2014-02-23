d78535910f564016b7d59cbbf13f1de526d403c6
Guy Kisel <gkisel@truecar.com>
2014-02-22 22:08:43 -0800

    Fixing setup.py to allow for repackaging on 3rd party PyPi servers

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
 
 from distutils.core import setup
 
+short_description = 'Robot Framework wrapper for faker, a fake test data generator'
+try:
+    description = open('README.md').read()
+except IOError:
+    description = short_description
+
+try:
+    license = open('LICENSE').read()
+except IOError:
+    license = 'MIT License'
+
 setup(
     name='robotframework-faker',
     packages=['robotframework-faker'],  # this must be the same as the name above
-    version='0.1',
-    description='Robot Framework wrapper for faker, a fake test data generator',
+    version='0.2',
+    description=short_description,
     author='Guy Kisel',
     author_email='guy.kisel@gmail.com',
     url='https://github.com/guykisel/robotframework-faker',
     keywords='robotframework testing test automation faker',  # arbitrary keywords
     classifiers=[],
     install_requires=['fake-factory', 'robotframework'],
-    long_description=open('README.md').read(),
-    license=open('LICENSE').read(),
+    long_description=description,
+    license=license,
 )

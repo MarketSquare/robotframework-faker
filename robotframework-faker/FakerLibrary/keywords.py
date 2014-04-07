e8efa5366a3edc75d8df09fb33c5cc2dfb225d7c
Guy Kisel <gkisel@truecar.com>
2014-04-07 15:48:31 -0700

    Autocast input based on best guess (issue #3)

--- robotframework-faker/FakerLibrary/keywords.py
+++ robotframework-faker/FakerLibrary/keywords.py
@@ -33,7 +33,44 @@ class FakerKeywords(object):
 
     def __getattr__(self, name):
         if name in _fake.__dict__:
-            return _fake.__dict__[name]
+            return _autocast(_fake.__dict__[name])
         elif name in faker.generator.Generator.__dict__:
-            return faker.generator.Generator.__dict__[name]
+            return _autocast(faker.generator.Generator.__dict__[name])
         raise AttributeError('Non-existing keyword "{}"'.format(name))
+
+
+#
+# From https://stackoverflow.com/questions/7019283/automatically-type-cast-parameters-in-python
+# by sequenceGeek (https://stackoverflow.com/users/525763/sequencegeek)
+# (stackoverflow content is cc-wiki (aka cc-by-sa) licensed)
+#
+def _boolify(s):
+    if s == 'True' or s == 'true':
+        return True
+    if s == 'False' or s == 'false':
+        return False
+    raise ValueError('Not Boolean Value!')
+
+
+def _estimateType(var):
+    '''guesses the str representation of the variables type'''
+    var = str(var)  #important if the parameters aren't strings...
+    for caster in (_boolify, int, float):
+        try:
+            return caster(var)
+        except ValueError:
+            pass
+    return var
+
+
+def _autocast(dFxn):
+    def wrapped(*c, **d):
+        cp = [_estimateType(x) for x in c]
+        dp = dict((i, _estimateType(j)) for (i, j) in d.items())
+        return dFxn(*cp, **dp)
+
+    return wrapped
+
+#
+# End of stackoverflow content
+#

f1f756d491b011a1e11dfdbfe648bb78c18a41d6
Guy Kisel <gkisel@truecar.com>
2014-08-19 17:43:01 -0700

    Unbreaking stuff, but also losing signatures

--- robotframework-faker/FakerLibrary/keywords.py
+++ robotframework-faker/FakerLibrary/keywords.py
@@ -2,7 +2,6 @@ import ast
 
 import faker
 import faker.generator
-import decorator
 from robot.api import logger
 
 """
@@ -12,9 +11,6 @@ via FakerLibrary calls in Robot Framework.
 
 """
 
-# create our faker
-_fake = faker.Factory.create()
-
 
 class FakerKeywords(object):
     """ 
@@ -23,25 +19,30 @@ class FakerKeywords(object):
     """
     
     ROBOT_LIBRARY_SCOPE = 'Global'
+    _fake = faker.Faker()
 
     def __init__(self, locale=None, providers=None, seed=None):
-        global _fake
-        _fake = faker.Factory.create(locale, providers)
+        self._fake = faker.Faker(locale, providers)
         if seed:
-            _fake.seed(seed)
+            self._fake.seed(seed)
 
     def get_keyword_names(self):
-        keywords = [name for name, function in _fake.__dict__.items() if hasattr(function, '__call__')]
-        keywords.extend(
-            [name for name, function in faker.generator.Generator.__dict__.items() if
-             hasattr(function, '__call__')])
+        keywords = [name for name, function in self._fake.__dict__.items() if
+                    hasattr(function, '__call__')]
+
+        keywords.extend([name for name, function in
+                         faker.generator.Generator.__dict__.items() if
+                         hasattr(function, '__call__')])
         return keywords
 
     def __getattr__(self, name):
-        if name in _fake.__dict__.keys():
-            return _str_vars_to_data(_fake.__dict__[name].im_func)
+        func = None
+        if name in self._fake.__dict__.keys():
+            func = getattr(self._fake, name)
         elif name in faker.generator.Generator.__dict__.keys():
-            return _str_vars_to_data(faker.generator.Generator.__dict__[name])
+            func = faker.generator.Generator.__dict__[name]
+        if func:
+            return _str_vars_to_data(func)
         raise AttributeError('Non-existing keyword "{0}"'.format(name))
 
 
@@ -52,10 +53,11 @@ def _str_to_data(string):
         return string
 
 
-@decorator.decorator
-def _str_vars_to_data(f, *args, **kwargs):
-    args = [_str_to_data(arg) for arg in args]
-    kwargs = dict((arg_name, _str_to_data(arg)) for arg_name, arg in kwargs.items())
-    result = f(*args, **kwargs)
-    logger.debug(result)
-    return result
+def _str_vars_to_data(f):
+    def wrapped(*args, **kwargs):
+        args = [_str_to_data(arg) for arg in args]
+        kwargs = dict((arg_name, _str_to_data(arg)) for arg_name, arg in kwargs.items())
+        result = f(*args, **kwargs)
+        logger.debug(result)
+        return result
+    return wrapped

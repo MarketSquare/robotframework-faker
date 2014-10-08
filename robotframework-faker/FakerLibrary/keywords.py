39f291a32e6b5d55a88ec0f8edee7d166dd72ef8
gkisel <gkisel@truecar.com>
2014-10-08 14:42:29 -0700

    Disable auto casting of strings to other primitive datatypes

--- robotframework-faker/FakerLibrary/keywords.py
+++ robotframework-faker/FakerLibrary/keywords.py
@@ -1,8 +1,5 @@
-import ast
-
 import faker
 import faker.generator
-from robot.api import logger
 
 """
 
@@ -13,11 +10,11 @@ via FakerLibrary calls in Robot Framework.
 
 
 class FakerKeywords(object):
-    """ 
-    This looks tricky but it's just the Robot Framework Hybrid Library API. 
+    """
+    This looks tricky but it's just the Robot Framework Hybrid Library API.
     http://robotframework.googlecode.com/hg/doc/userguide/RobotFrameworkUserGuide.html#hybrid-library-api
     """
-    
+
     ROBOT_LIBRARY_SCOPE = 'Global'
     _fake = faker.Faker()
 
@@ -42,24 +39,5 @@ class FakerKeywords(object):
         elif name in faker.generator.Generator.__dict__.keys():
             func = faker.generator.Generator.__dict__[name]
         if func:
-            # when running libdoc, need to disable this decorator temporarily
-            # to allow for RF's introspection
-            return _str_vars_to_data(func)
+            return func
         raise AttributeError('Non-existing keyword "{0}"'.format(name))
-
-
-def _str_to_data(string):
-    try:
-        return ast.literal_eval(str(string).strip())
-    except Exception:
-        return string
-
-
-def _str_vars_to_data(f):
-    def wrapped(*args, **kwargs):
-        args = [_str_to_data(arg) for arg in args]
-        kwargs = dict((arg_name, _str_to_data(arg)) for arg_name, arg in kwargs.items())
-        result = f(*args, **kwargs)
-        logger.debug(result)
-        return result
-    return wrapped

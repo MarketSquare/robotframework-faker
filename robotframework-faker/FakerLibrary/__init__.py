b5126b301f921b47567a89d1ce794f1e58a4a5c4
gkisel <gkisel@truecar.com>
2014-10-09 14:50:39 -0700

    Add FakerLibrary.__version__

--- robotframework-faker/FakerLibrary/__init__.py
+++ robotframework-faker/FakerLibrary/__init__.py
@@ -1,6 +1,11 @@
 __author__ = 'gkisel'
 
+import pkg_resources
+
 from keywords import FakerKeywords
+
+
+__version__ = pkg_resources.get_distribution("robotframework-faker").version
 
 
 class FakerLibrary(FakerKeywords):

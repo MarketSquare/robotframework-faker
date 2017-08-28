8c862ebf35c996d9257b0e8d795e8867fc30a0e2
Guy Kisel <guykisel@users.noreply.github.com>
2017-08-28 16:06:30 -0700

    use tox to support testing multiple python versions (#22)
    * update requirements
    
    * use tox
    
    * delete tox comment
    
    * don't need to install 3.6 explicitly
    
    * use tox-travis
    
    * add bumpversion config
    
    * faker 0.8.0 just got released and breaks us
    
    * add more config files

--- robotframework-faker/FakerLibrary/__init__.py
+++ robotframework-faker/FakerLibrary/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 import pkg_resources
 from .keywords import FakerKeywords
 
--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ try:
     description = open('README.rst').read()
 except IOError:
     description = short_description
-    
-    
+
+
 classifiers = """
 Development Status :: 5 - Production/Stable
 License :: OSI Approved :: MIT License
@@ -15,6 +15,10 @@ Operating System :: OS Independent
 Programming Language :: Python
 Programming Language :: Python :: 2
 Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.6
 Topic :: Software Development :: Testing
 Topic :: Software Development :: Quality Assurance
 """.strip().splitlines()

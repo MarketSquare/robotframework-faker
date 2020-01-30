557c6f203bb7685f3fd06e1b1f4af9c0f486a564
Laurent Bristiel <laurent@bristiel.com>
2020-01-30 09:12:55 +0100

    Bump version to 5.0.0 using faker 4.0.0 that removes support for Python 2.7

--- robotframework-faker/FakerLibrary/__init__.py
+++ robotframework-faker/FakerLibrary/__init__.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import
-
 import pkg_resources
 from .keywords import FakerKeywords
 
--- robotframework-faker/__init__.py
+++ robotframework-faker/__init__.py
@@ -3,4 +3,4 @@ from __future__ import unicode_literals
 
 __author__ = "Guy Kisel"
 __email__ = "guy.kisel@gmail.com"
-__version__ = "4.3.0"
+__version__ = "5.0.0"

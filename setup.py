c66cb1e66a0ef2f04aa072c7f281b11055c0af8c
Ed Manlove <emanlove@verizon.net>
2025-07-05 08:02:18 -0400

    Update supported Python versions

--- setup.py
+++ setup.py
@@ -17,10 +17,11 @@ Operating System :: OS Independent
 Programming Language :: Python
 Programming Language :: Python :: 3
 Programming Language :: Python :: 3 :: Only
-Programming Language :: Python :: 3.5
-Programming Language :: Python :: 3.6
-Programming Language :: Python :: 3.7
-Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
+Programming Language :: Python :: 3.10
+Programming Language :: Python :: 3.11
+Programming Language :: Python :: 3.12
+Programming Language :: Python :: 3.13
 Topic :: Software Development :: Testing
 Topic :: Software Development :: Quality Assurance
 """.strip().splitlines()

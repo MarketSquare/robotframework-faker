ce9951d97855b34ec32654a4baf87bb85c34b208
Ed Manlove <emanlove@verizon.net>
2025-07-15 21:37:29 -0400

    Added docstring to main library
    This will added some information to the keyword docs including noting which version of
    Faker the docs were built against.

--- robotframework-faker/FakerLibrary/__init__.py
+++ robotframework-faker/FakerLibrary/__init__.py
@@ -11,3 +11,9 @@ class FakerLibrary(FakerKeywords):
     """
 
     ROBOT_LIBRARY_SCOPE = "GLOBAL"
+    __doc__ = f"""
+    Robot Framework keyword library wrapper for Faker.
+
+    This set of keywords was generated using Faker version {version("Faker")}.
+    """
+
